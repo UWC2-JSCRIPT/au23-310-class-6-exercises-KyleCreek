22d1415884750b9e1af568fdf9bb00eac2dbd6a6
KyleCreek <kyle.a.creek@gmail.com>
2023-11-14 19:05:09 -0800

    Finished carProto.js

--- carProto.js
+++ carProto.js
@@ -5,11 +5,55 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
-
+class Car{
+    constructor(model){
+        this.model = model;
+        this.currentSpeed = 0;
+    }
+    // Increases currentSpeed by 1
+    accelerate(){
+        this.currentSpeed += 1;
+    }
+    // Decreases currentSpeed by 1
+    brake(){
+        this.currentSpeed -= 1;
+    }
+    // String Representation of Model. 
+    toString(){
+        console.log(`Model: ${this.model} | Travelling Speed: ${this.currentSpeed}`);
+    }
+}
 /**
  * ElectricCar class
  * @constructor
  * @param {String} model
  */
+// Note to Kyle; This needs work prior to Submission. 
+class ElectricCar extends Car{
+    constructor(model, currentSpeed){
+        super(model, currentSpeed);
+        this.motor = 'electric';
+    }
+    accelerate(){
+        super.accelerate();
+        super.accelerate();
+    }
+    toString(){
+        console.log(`Electric Car | Current Speed: ${this.currentSpeed} | Model: ${this.model}`);
+    }
+}
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+
+const clownCar = new Car('Clown');
+clownCar.accelerate();
+clownCar.accelerate();
+clownCar.brake();
+clownCar.toString();
+
+const electricCar = new ElectricCar('Electric');
+electricCar.accelerate();
+electricCar.accelerate();
+electricCar.brake();
+electricCar.toString();
+
