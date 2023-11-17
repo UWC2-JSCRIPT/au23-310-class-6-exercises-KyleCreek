3b89f32d4775e00a25a1c6d04a654e0167b9ceb8
KyleCreek <kyle.a.creek@gmail.com>
2023-11-16 20:44:08 -0800

    Working on To Do list

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -12,8 +12,23 @@
 const addListItem = function(e) {
   e.preventDefault();
   const text = $('input').val();
+  // Build List Item
+  const newLi = $('<li>');
+  // Build Span. 
+  const newSpan = $('<span>');
+  newSpan.text(text);
+  
+  // Build Delete Buttton
+  const newA = $('<a>');
+  newA.text('Delete');
+  newA.addClass('delete');
+
+  // Append the Elements
+  newLi.append(newSpan);
+  newLi.append(newA);
 
   // rest here...
 };
 
 // add listener for add
+$('.add-item').on('click', addListItem);
