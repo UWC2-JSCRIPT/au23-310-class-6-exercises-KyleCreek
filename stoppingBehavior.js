d7047aefb95c61f6a82c0156572230a88c5022a9
KyleCreek <kyle.a.creek@gmail.com>
2023-11-16 19:44:55 -0800

    Completed stoppingBehavior

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,3 +1,4 @@
+$( document ).ready(function(){
 // Do not change
 document.getElementById('cat').addEventListener('click', () => {
   alert('meow!');
@@ -5,6 +6,16 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
-
+$('#more-info').on("click", function(e){
+  console.log(e);
+  e.preventDefault();
+});
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+$('#dog').on("click", function(e){
+  e.stopPropagation();
+  alert("Bow Wow");
+})
+
+});
